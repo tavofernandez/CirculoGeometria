37c43af3f6fda793cf8e8becede351223c106b81
Gustavo Fernandez <tavovicious@gmail.com>
2024-09-24 23:00:59 -0500

    Se diseña la interfaz grafica de usuario

--- frmCirculo.cs
+++ frmCirculo.cs
@@ -15,5 +15,10 @@ namespace CirculoGeometria
         {
             InitializeComponent();
         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
     }
 }
